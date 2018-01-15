30582c5fc19c259d561538a58115800d317481f8
Zachary Rosenlund <zrosenlund@mail.greenriver.edu>
2018-01-15 16:43:26 -0500

    created associative array and printed out checkbox inputs using a foreach loop

--- array-review.php
+++ array-review.php
@@ -9,6 +9,9 @@ ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
 $animals = array('panda', 'alpaca', 'boa');
+$flavors = array('grasshopper'=>'The Grasshopper', 'maple'=>'Whiskey Maple Bacon',
+    'carrot'=>'Carrot Walnut', 'caramel'=>'Salted Caramel Cupcake', 'velvet'=>'Red Velvet',
+    'lemon'=>'Lemon Drop', 'tiramisu'=>'Tiramisu');
 
 function printArray() {
     global $animals;
@@ -39,3 +42,10 @@ echo '<h2>Part I</h2>';
 printArray();
 addAnimal('goat');
 addAnimal('Boa');
+
+echo '<h2>Part II</h2>';
+
+foreach($flavors as $flavor => $name)
+{
+    echo "<input type='checkbox' name='flavors[]' value='$flavor'> $name<br>";
+}
