20bc65fe5d6b8d8b06bc83c6c43cb1bad9d081aa
Zachary Rosenlund <zrosenlund@mail.greenriver.edu>
2018-01-15 16:32:06 -0500

    created array

---
+++ array-review.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zrose
+ * Date: 1/15/2018
+ * Time: 12:37 PM
+ */
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+$animals = array('panda', 'alpaca', 'boa');
